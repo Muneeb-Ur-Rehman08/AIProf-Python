8d9475fae6427a907caff1c2ba31687495321f42
Muneeb Rehman <117652966+MuneebRehman8@users.noreply.github.com>
2025-02-08 20:07:01 +0500

    Update asgi.py

--- app/asgi.py
+++ app/asgi.py
@@ -11,12 +11,14 @@ import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from assistantchat.routing import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 django.setup()
+
+# Move these imports AFTER django.setup()
+from assistantchat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
